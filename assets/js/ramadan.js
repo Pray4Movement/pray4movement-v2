938cbdbac026aa776534313fb2197006cb3ca0df
corsacca <corsacca@gmail.com>
2021-12-08 15:54:54 +0100

    Display correct link and campaign progress

--- assets/js/ramadan.js
+++ assets/js/ramadan.js
@@ -5,19 +5,25 @@ jQuery(document).ready(function($) {
    window.mapbox_library_api.get_level = function (){
      return 'world';
    }
-    console.log(window.p4m_ramadan);
     window.mapbox_library_api.area_map.load_detail_panel = function(location_id, name){
         let location_data = window.p4m_ramadan.data.locations[location_id] || []
         let initiatives = location_data.initiatives || []
-        console.log(location_data);
         $('#geocode-details-title').html( name )
         let content_html = `<ul>`
         if ( initiatives.length === 0 ){
             content_html += `<li>No prayer initiatives here yet</li>`
         }
         initiatives.forEach(initiative=>{
-            if ( initiative.link){
-                content_html += `<li><a href="${window.lodash.escape(initiative.link)}">${window.lodash.escape(initiative.label)}</a></li>`
+            let link = initiative.campaign_link || initiative.initiative_link
+            if ( initiative.campaign_progress && !isNaN(initiative.campaign_progress)){
+                initiative.campaign_progress = ` - ${initiative.campaign_progress}%`
+            }
+            if ( link ){
+                content_html += `<li>
+                    <a target="_blank" href="${window.lodash.escape(link)}">
+                        ${window.lodash.escape(initiative.label)} ${window.lodash.escape(initiative.campaign_progress)}
+                    </a>
+                </li>`
             } else {
                 content_html += `<li>${window.lodash.escape(initiative.label)}</li>`
             }
