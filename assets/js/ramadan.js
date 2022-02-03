386e5e907b8433a53cad99295958c314d65259e7
corsacca <corsacca@gmail.com>
2022-02-03 13:03:29 +0100

    Map: prioritize campaign link only on ramadan page

--- assets/js/ramadan.js
+++ assets/js/ramadan.js
@@ -14,7 +14,7 @@ jQuery(document).ready(function($) {
             content_html += `<li>No prayer initiatives here yet</li>`
         }
         initiatives.forEach(initiative=>{
-            let link = initiative.campaign_link || initiative.initiative_link
+            let link = window.p4m_ramadan.type === "ramadan" ? ( initiative.campaign_link || initiative.initiative_link ) : ( initiative.initiative_link || initiative.campaign_link );
             if ( initiative.campaign_progress && !isNaN(initiative.campaign_progress)){
                 initiative.campaign_progress = ` - ${initiative.campaign_progress}%`
             }
